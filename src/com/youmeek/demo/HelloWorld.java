4a3c875ae91b728f3915afcd228c4ba66745ff34
陈文杰 <wutianba@sina.com>
2018-04-25 16:54:10 +0800

    Initial commit

---
+++ src/com/youmeek/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.youmeek.demo;
+
+/**
+ * Created by chenwenjie on 2018/4/25.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
